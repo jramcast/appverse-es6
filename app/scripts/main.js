3b41e445be16b93c47a1fef03d13640182a4cc40
Jaime Ramírez Castillo <jere@gft.com>
2014-12-22 13:49:30 +0000

    First commit

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+class Greeter {
+    constructor(message) {
+      this.message = message;
+    }
+
+    greet() {
+      var element = document.querySelector('#message');
+      element.innerHTML = this.message;
+    }
+}
+
+var greeter = new Greeter('Hello, world!');
+greeter.greet();
+
+
